869d18e00bb2929aebaabcaef566eedf80bb1cc0
Luisil41 <luisisarrial@gmail.com>
2022-07-01 20:52:56 +0200

    first commit

---
+++ abstraccion.py
@@ -0,0 +1,20 @@
+class Calculadora():
+    memoria = []
+
+    def __init__(self, n1, n2):
+        print(n1 + n2)
+        print(n1 - n2)
+        print(n1 * n2)
+        print(n1 / n2)
+
+    def suma(self, n1, n2):
+        return n1 + n2
+
+    def suma(self, n1, n2):
+        return n1 - n2
+
+    def suma(self, n1, n2):
+        return n1 * n2
+
+    def suma(self, n1, n2):
+        return n1 / n2
