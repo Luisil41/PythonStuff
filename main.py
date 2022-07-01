917bb40abafe1b2b4eeda11268e2343bc998545b
Luisil41 <luisisarrial@gmail.com>
2022-07-01 20:33:26 +0200

    Methods changed

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ class Alumno():
     apellidos = ""
     dnit = ""
     edad = 0
-    nota = 0
+    asignaturas = []
 
     # Constructor
     def __init__(self, nombre, apellidos, dni, edad):
@@ -17,9 +17,25 @@ class Alumno():
     def saludar(self):
         print(f'Hola, mi nombre es {self.nombre}')
 
-    def añadir_nota(self):
+    def cumpleaños(self):
         self.edad += 1
 
-    def cumplir_años(self, nota):
+    def añadir_asignatura(self, asignatura):
+        self.asignaturas.append(asignatura)
+
+
+
+class Asignatura():
+    #Propiedades
+    nombre = ""
+    nota = 0
+
+    #Constructor
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+
+    #Método
+    def añadir_nota(self, nota):
         if nota >= 0 and nota <= 10:
-            self.nota = nota
+        self.nota = nota
